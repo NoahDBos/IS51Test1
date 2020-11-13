ccdd071057319452c5ba50a81766954b4261b2fa
NoahDBos <70241751+NoahDBos@users.noreply.github.com>
2020-11-12 16:49:43 -0800

    structured english

---
+++ IS51Test1.py
@@ -0,0 +1,15 @@
+
+"""
+Create a program that will determine which option results in more money.
+Option one is $100 a for ten days. Option 2 starts at $1 day but doubles
+each day for ten days. One function will determine the amount of pay for
+Option 1. The second function will determine the amount of pay for Option 2. 
+
+Function 1 will return 100 * 10 days
+Function 2 will return a loop consisting of 10 times and each time it will double
+
+It will then evaluate the results of Option 1 and Option 2. If they are equal 
+it will tell the user "Option 1 and Option 2 pay the same"
+if Option 1 is more it will say "Option 1 is better"
+if Option 2 is more it will say "Option 2 is better"
+"""
